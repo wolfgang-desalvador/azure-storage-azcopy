2d8f9a3e0b2af6fa262c2570db03daf8287f7a4d
Dhanashree Phulkar <dphulkar@microsoft.com>
2025-04-01 11:46:26 +0530

    version update

--- common/version.go
+++ common/version.go
@@ -1,10 +1,6 @@
 package common
 
-<<<<<<< HEAD
-const AzcopyVersion = "10.28.1~preview"
-=======
 const AzcopyVersion = "10.29.0-Preview"
->>>>>>> edb71da0a00b05b01321756da17ec827edea1d43
 const UserAgent = "AzCopy/" + AzcopyVersion
 const S3ImportUserAgent = "S3Import " + UserAgent
 const GCPImportUserAgent = "GCPImport " + UserAgent
